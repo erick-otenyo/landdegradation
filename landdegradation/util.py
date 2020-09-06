348489f9137bd64aba1d9ddb1a508c0e8e19461d
Erick Otenyo <otenyo.erick@gmail.com>
2020-09-06 17:25:20 +0300

    Update util.py

--- landdegradation/util.py
+++ landdegradation/util.py
@@ -91,7 +91,6 @@ class gee_task(threading.Thread):
 
     def get_urls(self):
         resp = requests.get('https://www.googleapis.com/storage/v1/b/{bucket}/o?prefix={prefix}'.format(bucket=BUCKET, prefix=self.prefix))
-        print(resp.json())
         if not resp or resp.status_code != 200:
             raise GEETaskFailure('Failed to list urls for results from {}'.format(self.task))
 
